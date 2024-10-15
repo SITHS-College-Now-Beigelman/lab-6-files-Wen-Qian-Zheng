33fec8715330577bdbf38a1208254d7e5f3c78b2
Wen Qian Zheng <158204243+Wen-Qian-Zheng@users.noreply.github.com>
2024-10-15 15:58:56 -0400

    Update Zheng_Wen_Qian_Lab_6.cpp

--- Zheng_Wen_Qian_Lab_6.cpp
+++ Zheng_Wen_Qian_Lab_6.cpp
@@ -7,21 +7,27 @@ October 7th, 2024*/
 #include <string>
 using namespace std;
 
-int main () {
+int main ()
+{
+    // Variables (Files, Integers, Characters, and Strings)
     ifstream inFile;
     ofstream outFile;
     int integer1, integer2;
     char character1;
     string string1;
 
+     //Open Files as Variables
     inFile.open("inData.txt");
     outFile.open("outData.txt");
-
+    
+    //Outputs Values (Modified) to outData.txt
     inFile >> integer1 >> integer2 >> character1 >> string1;
     outFile << integer1 + integer2 << "\n" << int(character1) + 1 << "\n" << string1;
 
+    //Closes Files
     inFile.close();
     outFile.close();
 
+    //Exits Successfully
     return 0;
 }
